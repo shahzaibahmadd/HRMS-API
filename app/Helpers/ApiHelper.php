c2ec0ee2036436d35bbea072e2c46dbf42114e82
shahzaibahmadd <130222586+shahzaibahmadd@users.noreply.github.com>
2025-08-06 12:23:19 +0500

    Updated

---
+++ app/Helpers/ApiHelper.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Helpers;
+
+use App\DTOs\API\ApiDTOs;
+use App\Models\ApiLog;
+use App\services\ErrorLogging\ErrorLoggingService;
+
+
+class ApiHelper
+{
+
+    public static function logApi(ApiDTOs $dto): void
+    {
+        try {
+            ApiLog::create($dto->toArray());
+        } catch (\Throwable $e) {
+            ErrorLoggingService::log($e);
+        }
+    }
+
+}
